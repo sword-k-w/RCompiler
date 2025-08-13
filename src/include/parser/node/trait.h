1f73b2f72f96905fdc0177789bb70f0a965eedc5
sword <sword-k@sjtu.edu.cn>
2025-08-13 20:55:00 +0800

    implement the parsing of type param bounds

--- src/include/parser/node/trait.h
+++ src/include/parser/node/trait.h
@@ -11,12 +11,44 @@ class TraitBoundNode : public ASTNode {
 public:
   TraitBoundNode() = delete;
   TraitBoundNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  bool parenthesis_ = false;
+  bool question_ = false;
+  TypePathNode *type_path_ = nullptr;
+};
+
+class UseBoundGenericArgNode : public ASTNode {
+public:
+  UseBoundGenericArgNode() = delete;
+  UseBoundGenericArgNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  IdentifierNode *identifier_ = nullptr;
+  SelfUpperNode *self_upper_ = nullptr;
+};
+
+class UseBoundGenericArgsNode : public ASTNode {
+public:
+  UseBoundGenericArgsNode() = delete;
+  UseBoundGenericArgsNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  std::vector<UseBoundGenericArgNode *> use_bound_generic_arg_s_;
+  uint32_t comma_cnt_ = 0;
+};
+
+class UseBoundNode : public ASTNode {
+public:
+  UseBoundNode() = delete;
+  UseBoundNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+  UseBoundGenericArgsNode *use_bound_generic_args_ = nullptr;
 };
 
 class TypeParamBoundNode : public ASTNode {
 public:
   TypeParamBoundNode() = delete;
   TypeParamBoundNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  TraitBoundNode *trait_bound_ = nullptr;
+  UseBoundNode *use_bound_ = nullptr;
 };
 
 class TypeParamBoundsNode : public ASTNode {
@@ -24,6 +56,8 @@ public:
   TypeParamBoundsNode() = delete;
   TypeParamBoundsNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
 private:
+  std::vector<TypeParamBoundNode *> type_param_bound_s_;
+  uint32_t plus_cnt_ = 0;
 };
 
 class TraitNode : public ASTNode {
