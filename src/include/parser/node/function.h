784b80f44487132c94401e4bf9a313daca293bf7
sword <sword-k@sjtu.edu.cn>
2025-08-14 10:52:03 +0800

    implement the parsing of function-related nodes

--- src/include/parser/node/function.h
+++ src/include/parser/node/function.h
@@ -7,16 +7,71 @@
 #include "parser/node/generic.h"
 #include "parser/node/expression.h"
 
+class ShorthandSelfNode : public ASTNode {
+public:
+  ShorthandSelfNode() = delete;
+  ShorthandSelfNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  bool quote_ = false;
+  bool mut_ = false;
+  SelfLowerNode *self_lower_ = nullptr;
+};
+
+class TypedSelfNode : public ASTNode {
+public:
+  TypedSelfNode() = delete;
+  TypedSelfNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  bool mut_ = false;
+  SelfLowerNode *self_lower_ = nullptr;
+  TypeNode *type_ = nullptr;
+};
+
+class SelfParamNode : public ASTNode {
+public:
+  SelfParamNode() = delete;
+  SelfParamNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  ShorthandSelfNode *shorthand_self_ = nullptr;
+  TypedSelfNode *typed_self_ = nullptr;
+};
+
+class FunctionParamPatternNode : public ASTNode {
+public:
+  FunctionParamPatternNode() = delete;
+  FunctionParamPatternNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  PatternNoTopAltNode *pattern_no_top_alt_ = nullptr;
+  TypeNode *type_ = nullptr;
+  bool ellipsis_ = false;
+};
+
+class FunctionParamNode : public ASTNode {
+public:
+  FunctionParamNode() = delete;
+  FunctionParamNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  FunctionParamPatternNode *function_param_pattern_ = nullptr;
+  bool ellipsis_ = false;
+  TypeNode *type_ = nullptr;
+};
+
 class FunctionParametersNode : public ASTNode {
 public:
   FunctionParametersNode() = delete;
   FunctionParametersNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  SelfParamNode *self_param_ = nullptr;
+  uint32_t comma_cnt_ = 0;
+  std::vector<FunctionParamNode *> function_params_;
 };
 
 class FunctionReturnTypeNode : public ASTNode {
 public:
   FunctionReturnTypeNode() = delete;
   FunctionReturnTypeNode(const std::vector<Token> &, uint32_t &, const uint32_t &);
+private:
+  TypeNode *type_;
 };
 
 class FunctionNode : public ASTNode {
--- src/parser/node/function.cpp
+++ src/parser/node/function.cpp
@@ -1,5 +1,145 @@
 #include "parser/node/function.h"
 
+ShorthandSelfNode::ShorthandSelfNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Shorthand Self") {
+  try {
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme == "&") {
+      quote_ = true;
+      ++pos;
+      CheckLength(pos, length);
+    }
+    if (tokens[pos].lexeme == "mut") {
+      mut_ = true;
+      ++pos;
+      CheckLength(pos, length);
+    }
+    self_lower_ = node_pool.Make<SelfLowerNode>(tokens, pos, length);
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
+TypedSelfNode::TypedSelfNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Typed Self") {
+  try {
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme == "mut") {
+      mut_ = true;
+      ++pos;
+      CheckLength(pos, length);
+    }
+    self_lower_ = node_pool.Make<SelfLowerNode>(tokens, pos, length);
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme != ":") {
+      throw Error("try parsing Typed Self Node but no :");
+    }
+    ++pos;
+    type_ = node_pool.Make<TypeNode>(tokens, pos, length);
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
+SelfParamNode::SelfParamNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Self Param") {
+  try {
+    uint32_t tmp = pos;
+    try {
+      shorthand_self_ = node_pool.Make<ShorthandSelfNode>(tokens, pos, length);
+    } catch (...) {
+      shorthand_self_ = nullptr;
+      pos = tmp;
+      typed_self_ = node_pool.Make<TypedSelfNode>(tokens, pos, length);
+    }
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
+FunctionParamPatternNode::FunctionParamPatternNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Function Param Pattern") {
+  try {
+    pattern_no_top_alt_ = node_pool.Make<PatternNoTopAltNode>(tokens, pos, length);
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme != ":") {
+      throw Error("try parsing Function Param Pattern Node but no :");
+    }
+    ++pos;
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme == "...") {
+      ellipsis_ = true;
+      ++pos;
+    } else {
+      type_ = node_pool.Make<TypeNode>(tokens, pos, length);
+    }
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
+FunctionParamNode::FunctionParamNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Function Param") {
+  try {
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme == "...") {
+      ellipsis_ = true;
+      ++pos;
+    } else {
+      uint32_t tmp = pos;
+      try {
+        function_param_pattern_ = node_pool.Make<FunctionParamPatternNode>(tokens, pos, length);
+      } catch (...) {
+        function_param_pattern_ = nullptr;
+        pos = tmp;
+        type_ = node_pool.Make<TypeNode>(tokens, pos, length);
+      }
+    }
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
+FunctionParametersNode::FunctionParametersNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Function Parameters") {
+  try {
+    uint32_t tmp = pos;
+    try {
+      self_param_ = node_pool.Make<SelfParamNode>(tokens, pos, length);
+      CheckLength(pos, length);
+      if (tokens[pos].lexeme == ",") {
+        ++comma_cnt_;
+        ++pos;
+      }
+    } catch (...) {
+      self_param_ = nullptr;
+      comma_cnt_ = 0;
+      pos = tmp;
+      function_params_.push_back(node_pool.Make<FunctionParamNode>(tokens, pos, length));
+      while (pos < length && tokens[pos].lexeme != ")") {
+        if (tokens[pos].lexeme != ",") {
+          throw Error("try parsing Function Parameters Node but not ,");
+        }
+        ++comma_cnt_;
+        ++pos;
+        if (tokens[pos].lexeme == ")") {
+          break;
+        }
+        function_params_.push_back(node_pool.Make<FunctionParamNode>(tokens, pos, length));
+      }
+    }
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
+FunctionReturnTypeNode::FunctionReturnTypeNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Function Return Type") {
+  try {
+    CheckLength(pos, length);
+    if (tokens[pos].lexeme != "->") {
+      throw Error("try parsing Function Return Type Node but no ->");
+    }
+    ++pos;
+    type_ = node_pool.Make<TypeNode>(tokens, pos, length);
+  } catch (Error &err) {
+    throw err;
+  }
+}
+
 FunctionNode::FunctionNode(const std::vector<Token> &tokens, uint32_t &pos, const uint32_t &length) : ASTNode("Function") {
   try {
     CheckLength(pos, length);
