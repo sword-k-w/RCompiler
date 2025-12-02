3e0dd1b2429e1b3a7af12aba70bb9f782080d4a8
sword <sword-k@sjtu.edu.cn>
2025-12-02 22:16:34 +0800

    fixed the bug that the type of self parameter doesn't contains the info of whether it's reference

--- src/visitor/IR_generator/IR_generator.cpp
+++ src/visitor/IR_generator/IR_generator.cpp
@@ -559,9 +559,9 @@ void IRGenerator::Visit(FunctionNode *node) {
     if (self_param != nullptr) {
       std::string tmp = name_allocator_.Allocate("%tmp.");
       self_param->IR_name_ = name_allocator_.Allocate("%" + current_Self_.top()->identifier_->val_ + ".self");
-      cur_function_->AddParameter(std::make_shared<IRParameterNode>(
-        GetIRTypeString(self_param->type_info_.get()), tmp));
-      Borrow(self_param->IR_name_, tmp, GetIRTypeString(self_param->type_info_.get()));
+      std::string IR_type = self_param->shorthand_self_->quote_ ? "ptr" : GetIRTypeString(self_param->type_info_.get());
+      cur_function_->AddParameter(std::make_shared<IRParameterNode>(IR_type, tmp));
+      Borrow(self_param->IR_name_, tmp, IR_type);
     }
     for (auto &param : node->function_parameters_->function_params_) {
       auto pattern = param->pattern_no_top_alt_->identifier_pattern_;
--- test/IR_test.cpp
+++ test/IR_test.cpp
@@ -51,7 +51,7 @@ void TestCode(const std::string &code, std::ostream &out) {
 // }
 
 TEST(IRTest, MyTest) {
-  for (int t = 21; t <= 22; ++t) {
+  for (int t = 23; t <= 23; ++t) {
     std::cerr << "Testing my test" << t << "...\n";
     std::string input = LoadFromFile("tmp_data/" + std::to_string(t) + ".rx");
     std::string output_file = "tmp_data/" + std::to_string(t) + ".ll";
