2d0ddbe2700b9eaf3ce680f9f54b251c2f63eeb6
sword <sword-k@sjtu.edu.cn>
2025-12-17 08:30:05 +0800

    add Makefile to submit

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sword/swordProject/RCompiler
+# Build directory: /home/sword/swordProject/RCompiler
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
