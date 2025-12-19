7a5fd1bf956ae31550b6900288a2d9a0b36f32b3
sword <sword-k@sjtu.edu.cn>
2025-12-20 00:09:01 +0800

    add builtin declaration

--- src/IR_visitor/printer/IR_printer.cpp
+++ src/IR_visitor/printer/IR_printer.cpp
@@ -193,7 +193,7 @@ void IRPrinter::Visit(IRRootNode *node) {
   os_ << "declare i32 @getInt()\n";
   os_ << "declare void @printlnInt(i32)\n";
   os_ << "declare void @printInt(i32)\n";
-  os_ << builtin_ << '\n';
+  // os_ << builtin_ << '\n';
   for (auto &struct_node : node->structs_) {
     struct_node->Accept(this);
   }
